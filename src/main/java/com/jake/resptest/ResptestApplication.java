dcb8c6108c19eb5a266f50eb248efeae8a5c4aff
epike <epikeran@gmail.com>
2022-11-15 10:26:39 +0900

    Initial commit

---
+++ src/main/java/com/jake/resptest/ResptestApplication.java
@@ -0,0 +1,13 @@
+package com.jake.resptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResptestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResptestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jake/resptest/ResptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.resptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResptestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
